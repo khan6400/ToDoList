4900f317bd5a8f803d1e4f8a5b5762fc5cef3556
khan6400 <226998312+khan6400@users.noreply.github.com>
2025-08-20 01:22:30 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import to_do_list
+print("<<<<<<*************************************>>>>>>\n"
+    "<<<<<<****khan6400sTO DO LIST APP v1.0****>>>>>>\n"
+    "<<<<<<*************************************>>>>>>\n")
+print("---At any point enter 'quit' or 'exit' to exit the program---")
+while True: 
+    to_do_list.main_program()
